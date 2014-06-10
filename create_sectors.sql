142a6d8355cc45dcc630459f75e1ac9a5f291c1a
Thomas Brightwell <thomas.brightwell@brightnetwork.co.uk>
2014-06-10 11:23:22 +0100

    removing old filenames

--- create_sectors.sql
+++
@@ -1,8 +0,0 @@
-delimiter $$
-
-CREATE TABLE `sectors` (
-  `salesforce_sector_id` varchar(50) NOT NULL,
-  `sector` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`salesforce_sector_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
--- sectors_insert.sql
+++
@@ -1,9 +0,0 @@
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET0BEAX', 'Consumer & Retail');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET0GEAX', 'Professional Services');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET06EAH', 'Technology');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TESzXEAX', 'Charity & Public Sector');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET0VEAX', 'Consulting');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET0LEAX', 'Law');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TESzwEAH', 'Banking & Finance');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET01EAH', 'Entrepreneurship');
-insert into sectors (salesforce_sector_id, sector) values ('a0U2000000TET0QEAX', 'Marketing & Media');
