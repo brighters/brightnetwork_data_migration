43ccc1eed050cb82303f08f8a4bfdd92523133bd
Thomas Brightwell <thomas.brightwell@brightnetwork.co.uk>
2014-07-18 14:25:23 +0100

    make sure we join correctly

--- 41_extract_signup_date.sql
+++ 41_extract_signup_date.sql
@@ -1,5 +1,5 @@
 select u.id as "user_id", min(a.timestamp_from) as "signup_date" from "user" u 
-join activity a on u.id = a.user_id
+join activity a on u.id = a.resource_id_target
 where action_id = 1
 group by u.id
 order by min(a.timestamp_from)
