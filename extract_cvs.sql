86ea0a9f228b86bf25ccc7098af79f8b4d7795da
Thomas Brightwell <thomas.brightwell@brightnetwork.co.uk>
2014-07-18 14:26:56 +0100

    don't need that

--- extract_cvs.sql
+++
@@ -1,7 +0,0 @@
-select r.id, f.data, rfile.resource_type_id, ft.extension from resource r 
-join connection c on r.id = c.resource_id_from
-join resource rfile on c.resource_id_to = rfile.id and rfile.resource_type_id = 10
-join file f on rfile.id = f.resource_id
-join file_type ft on f.file_type_id = ft.id
-where r.resource_type_id = 38
-limit 100
